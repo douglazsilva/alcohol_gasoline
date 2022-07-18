f37906a114044a7d9e71a06837340015363e727e
Douglas Silva <fdcs@cesar.org.br>
2022-07-18 12:57:46 -0300

    get device platform

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,8 @@ import 'package:alcohol_gasoline/ui/android/my_material_app.dart';
 import 'package:alcohol_gasoline/ui/ios/my_cupertino_app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyCupertinoApp());
+import 'dart:io' show Platform;
+
+void main() => Platform.isIOS
+    ? runApp(const MyCupertinoApp())
+    : runApp(const MyMaterialApp());
